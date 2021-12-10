6fab67a1241b783d6018cc93dffd02f0a3088888
rodolfo-pedroza <rudy_pedroza@hotmail.com>
2021-12-09 23:13:20 -0600

    mejores validaciones

--- src/views/Signup.vue
+++ src/views/Signup.vue
@@ -4,17 +4,29 @@
           <div class="text-center mb-4">
               <h1>Sign Up</h1>
           </div>
-          <form id="login-form">
+          <form id="login-form" @submit.prevent="handleSubmit">
                 <div class="mb-4">
                     <label for="email">Dirección de Correo</label>
-                    <input type="email" class="form-control form-control-lg" required>
+                    <input type="email" v-model="state.email" class="form-control form-control-lg" >
+                    <span v-if="v$.email.$error"> {{v$.email.$errors[0].$message}} </span>
+                </div>
+                <div class="mb-4">
+                    <label for="nombre">Usuario</label>
+                    <input type="text" v-model="state.nombre" class="form-control form-control-lg"  >
+                    <span v-if="v$.nombre.$error"> {{v$.nombre.$errors[0].$message}} </span>
                 </div>
                 <div class="mb-4">
                     <label for="password">Contraseña </label>
-                    <input type="password" class="form-control form-control-lg" minlength="8" maxlength="16" required pattern="^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?!.*\s).*$" title="La contraseña debe contener Mayúsculas, Minúsculas, Números y un Mínimo de 8 Caracteres"> 
+                    <input type="password" v-model="state.password.password" class="form-control form-control-lg" > 
+                    <span v-if="v$.password.password.$error"> {{v$.password.password.$errors[0].$message}}</span>
                 </div>
                 <div class="mb-4">
-                    <button class="btn btn-primary btn-lg col-sm-6">Sign Up</button>
+                    <label for="passwordConfirm">Confirmar Contraseña </label>
+                    <input type="password" v-model="state.password.repeatPassword" class="form-control form-control-lg" onpaste="return false;" > 
+                    <span v-if="v$.password.repeatPassword.$error">  {{v$.password.repeatPassword.$errors[0].$message}} </span>
+                </div>
+                <div class="mb-4">
+                    <button @click="submitForm" class="btn btn-primary btn-lg col-sm-6">Sign Up</button>
                 </div>
             </form>
             <div class="mb-4">
@@ -22,11 +34,55 @@
             </div>
       </div>
   </div>
+
 </template>
 
 <script>
-export default {
 
+import useValidate from '@vuelidate/core'
+import { required, sameAs, minLength, email} from '@vuelidate/validators'
+import { computed, reactive } from '@vue/reactivity'
+
+export default {
+    setup() {
+        const state = reactive({
+            nombre: '',
+            email: '',
+            password: {
+                password: '',
+                repeatPassword: '',
+            },
+        })
+
+        const rules = computed(() => {
+            return {
+                nombre: {required},
+                email: {required, email},
+                password: {
+                    password: {required, minLength: minLength(8)},
+                    repeatPassword: {required, sameAs: sameAs(state.password.password)} ,
+                },
+            };
+        });
+
+        const v$ = useValidate(rules, state)
+
+        return{
+            state,
+            v$
+        }
+    },
+    methods: {
+        submitForm() {
+            this.v$.$validate()
+            if(!this.v$.$error){
+                alert('enviado')
+            }else{
+                alert('revisar los campos')
+            }
+            // console.log(this.v$)
+        },
+    },
 }
 </script>
 
